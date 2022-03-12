e724a73466e35ba2888c499f5c1a605b741f6059
AhmedTitef <57777850+AhmedTitef@users.noreply.github.com>
2022-03-11 20:42:31 -0800

    fixed errors with exchange

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -21,7 +21,7 @@ const Exchanges = () => {
                 <Col span={6}>Exchanges</Col>
                 <Col span={6}>24h Trade Volume</Col>
                 <Col span={6}>Markets</Col>
-                <Col span={6}>Change</Col>
+
             </Row>
             <Row>
                 {exchangesList.map((exchange) => (
@@ -37,13 +37,13 @@ const Exchanges = () => {
                                             <Avatar className="exchange-image" src={exchange.iconUrl} />
                                             <Text><strong>{exchange.name}</strong></Text>
                                         </Col>
-                                        <Col span={6}>${millify(exchange.volume)}</Col>
+                                        <Col span={6}>${millify(exchange["24hVolume"])}</Col>
                                         <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
-                                        <Col span={6}>{millify(exchange.marketShare)}%</Col>
+
                                     </Row>
                                 )}
                             >
-                                {HTMLReactParser(exchange.description || '')}
+
                             </Panel>
                         </Collapse>
                     </Col>
